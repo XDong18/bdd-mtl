ef123780bb190e651772b09517cc158f3f83e3cf
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-29 20:35:58 +0800

    cc

--- bdd_mtl/tools/dist_train.sh
+++ bdd_mtl/tools/dist_train.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-
+ROOT=/shared/xudongliu/code/bdd-mtl/bdd_mtl
 CONFIG=$1
 GPUS=$2
 FOLDER=work_dirs
 
 python3 -m torch.distributed.launch --nproc_per_node=$GPUS ⁠\
-./tools/train.py \
+${ROOT}/tools/train.py \
 ./configs/${CONFIG}.py \
 --work_dir=./${FOLDER}/${CONFIG} \
 --launcher pytorch
