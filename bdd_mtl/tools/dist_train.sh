c05b2c5ff92ef33a29448fa4d2d4f40e0fb5f79a
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-29 20:29:10 +0800

    cc

--- bdd_mtl/tools/dist_train.sh
+++ bdd_mtl/tools/dist_train.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
-ROOT=/mnt/lustre/pangjiangmiao/jiangmiao/Detect-Track
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 CONFIG=$1
 GPUS=$2
 FOLDER=work_dirs
 
 python3 -m torch.distributed.launch --nproc_per_node=$GPUS ⁠\
-${ROOT}/tools/train.py \
+./tools/train.py \
 ./configs/${CONFIG}.py \
 --work_dir=./${FOLDER}/${CONFIG} \
 --launcher pytorch
