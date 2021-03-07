4431778c63b9b41a9de0bfca70e73025925539f4
XDong18 <liuxd1215@gmail.com>
2021-03-07 14:35:46 +0800

    update

--- bdd_mtl/mmdet/core/track/mdat_eval.py
+++ bdd_mtl/mmdet/core/track/mdat_eval.py
@@ -1,6 +1,6 @@
 import motmetrics as mm
 from collections import defaultdict
-from pycocotools.bdd import BDD
+# from pycocotools.bdd import BDD
 from pycocotools.videoeval import VideoEval
 import numpy as np
 
@@ -31,28 +31,28 @@ def mask_iou_matrix(objs, hyps, max_iou=1.):
     C[C > max_iou] = np.nan
     return C.transpose()
 
-def get_track_ap(gt_file, results, with_occlusion=False, return_vis_info=False):
-    # bdd ground truth and detection files
-    bddGt = BDD(gt_file)
-    bddDt = bddGt.loadRes(results)
-    # prepare for returning for visualization
-    if return_vis_info:
-        out = dict()
+# def get_track_ap(gt_file, results, with_occlusion=False, return_vis_info=False):
+#     # bdd ground truth and detection files
+#     bddGt = BDD(gt_file)
+#     bddDt = bddGt.loadRes(results)
+#     # prepare for returning for visualization
+#     if return_vis_info:
+#         out = dict()
 
-    video_eval = VideoEval(bddGt, bddDt, iouType='bbox')
-    # without and with re-ID
-    video_eval.evaluate(with_occlusion=with_occlusion)
-    video_eval.accumulate()
-    video_eval.summarize()
-    if return_vis_info:
-        out['gt_tracks'] = video_eval._gts
-        out['dt_tracks'] = video_eval._dts
-        out['gt_matches'] = [evalVid['gtMatches'] if evalVid else None for evalVid in video_eval.evalVids]
-        out['gt_ids'] = [evalVid['gtIds'] if evalVid else None for evalVid in video_eval.evalVids]
-        out['dt_ids'] = [evalVid['dtIds'] if evalVid else None for evalVid in video_eval.evalVids]
+#     video_eval = VideoEval(bddGt, bddDt, iouType='bbox')
+#     # without and with re-ID
+#     video_eval.evaluate(with_occlusion=with_occlusion)
+#     video_eval.accumulate()
+#     video_eval.summarize()
+#     if return_vis_info:
+#         out['gt_tracks'] = video_eval._gts
+#         out['dt_tracks'] = video_eval._dts
+#         out['gt_matches'] = [evalVid['gtMatches'] if evalVid else None for evalVid in video_eval.evalVids]
+#         out['gt_ids'] = [evalVid['gtIds'] if evalVid else None for evalVid in video_eval.evalVids]
+#         out['dt_ids'] = [evalVid['dtIds'] if evalVid else None for evalVid in video_eval.evalVids]
 
-    if return_vis_info:
-        return out
+#     if return_vis_info:
+#         return out
 
 
 def mdat_eval(all_results, dataset, out_file, cfg=None, ann_file=None, return_vis_info=False, with_mask=False):
